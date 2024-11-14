f38cdb7a10de348810397f6243b57416cf13a987
Moh Solehuddin <moh.solehuddin190805@gmail.com>
2024-11-14 18:22:07 +0700

    config

--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,18 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: {
+    // proxy: {
+    //   "/api": {
+    //     // target: "http://10.10.102.92:8080",
+    //     target: "https://urgently-precious-spider.ngrok-free.app",
+    //     changeOrigin: true,
+    //   },
+    // },
     proxy: {
       "/api": {
-        // target: "http://10.10.102.92:8080",
         target: "https://urgently-precious-spider.ngrok-free.app",
         changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ""), // Menghapus prefix `/api`
       },
     },
   },
